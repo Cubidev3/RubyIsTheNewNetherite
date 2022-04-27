19152ff50ad0e1b0917ab8dfc2d38dabf5a51edf
cubidev3 <perdigaomaisamigo@gmail.com>
2022-04-27 07:03:38 -0300

    Added Ruby Block

--- src/main/kotlin/com/cubidev/ritnn/registry/ModBlocks.kt
+++ src/main/kotlin/com/cubidev/ritnn/registry/ModBlocks.kt
@@ -1,4 +1,25 @@
 package com.cubidev.ritnn.registry
 
-class ModBlocks {
+import com.cubidev.ritnn.Ritnn
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.impl.tag.convention.TagRegistration
+import net.minecraft.block.Block
+import net.minecraft.block.Material
+import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+
+object ModBlocks {
+    val RUBY_BLOCK = Block(FabricBlockSettings
+        .of(Material.METAL)
+        .strength(5.0f, 6.0f)
+        .requiresTool()
+        .sounds(BlockSoundGroup.METAL)
+    )
+
+    fun registerBlocks() {
+        Registry.register(Registry.BLOCK, Identifier(Ritnn.MOD_ID, "ruby_block"), RUBY_BLOCK)
+    }
 }
--- src/main/kotlin/com/cubidev/ritnn/registry/ModItems.kt
+++ src/main/kotlin/com/cubidev/ritnn/registry/ModItems.kt
@@ -1,6 +1,7 @@
 package com.cubidev.ritnn.registry
 
 import com.cubidev.ritnn.Ritnn
+import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.util.Identifier
@@ -11,7 +12,14 @@ object ModItems {
         Item.Settings().group(ItemGroup.MATERIALS)
     )
 
+    //Block Items
+    val RUBY_BLOCK_ITEM = BlockItem(
+        ModBlocks.RUBY_BLOCK,
+        Item.Settings().group(ItemGroup.BUILDING_BLOCKS)
+    )
+
     fun registerItems() {
         Registry.register(Registry.ITEM, Identifier(Ritnn.MOD_ID, "ruby"), RUBY)
+        Registry.register(Registry.ITEM, Identifier(Ritnn.MOD_ID, "ruby_block"), RUBY_BLOCK_ITEM)
     }
 }
